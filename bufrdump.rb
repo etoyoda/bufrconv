eb0db2f762266eff2b233bd26eb50fe83a4215ea
TOYODA Eizi <toyoda.eizi@gmail.com>
2019-10-28 00:24:54 +0000

    untested

--- bufrdump.rb
+++ bufrdump.rb
@@ -8,28 +8,53 @@ class BufrDecode
 
   def initialize tape, bufrmsg
     @tape, @bufrmsg = tape, bufrmsg
-    @ptr = nil
-    (0 ... @tape.size).each{|i| @tape[i][:pos] = i unless @tape[i][:pos]}
+    @pos = nil
+    # tape marker for debugging -- note a descriptor may appear twice or more
+    (0 ... @tape.size).each{|i|
+      @tape[i][:pos] = i unless @tape[i][:pos]
+    }
+    # replication counter: nesting implemented using stack push/pop
     @cstack = [{:type=>:dummy, :ctr=>0}]
   end
 
-  def read_tape
-    d = @tape[@ptr]
-    @ptr += 1
+  def rewind
+    @pos = 0
+  end
+
+  def forward n
+    @pos += n
+  end
+
+=begin
+記述子列がチューリングマシンのテープであるかのように読み出す。
+反復記述子がループ命令に相当する。反復記述子を読むと、
+反復数カウンタ clast[:niter] と
+記述子数カウンタ clast[:ctr] が設定される。
+ループ内の記述子を読み出した後で、記述子数カウンタは１減算される。
+記述子数カウンタをゼロにするとき、反復数カウンタが１減算される。
+反復数カウンタが１減算の結果非零になるならば、読み出し位置が戻される。
+記述子数カウンタのループ先頭での設定値や読み出し位置の戻し幅には、遅延反復記述子を計算に入れない。
+反復数カウンタが１減算の結果ゼロになるならば、反復が終了する。
+BUFRの反復はネストできなければいけないので（用例があるか知らないが）、カウンタ設定時に現在値はスタック構造で退避される。反復に入っていないときはダミーの記述子数カウンタが初期値ゼロで入っており、１減算によってゼロになることはない。
+=end
+
+  def read_tape(keep_ctr = false)
     clast = @cstack.last
+    p({:read_tape=>keep_ctr, :pos=>@pos, :cstack=>@cstack.size, :ctos=>clast}) if $VERBOSE
+    d = @tape[@pos]
+    @pos += 1
+    return d if keep_ctr
     clast[:ctr] -= 1
     if clast[:ctr].zero? then
       clast[:niter] -= 1
       if clast[:niter].zero? then
         @cstack.pop
-        p({:newptr=>@ptr, :endrepl=>clast}) if $VERBOSE
+        p({:cstack_pop=>clast, :pos=>@pos}) if $VERBOSE
       else
         clast[:ctr] = clast[:ndesc] + 1
-        @ptr -= clast[:ndesc]
-        p({:newptr=>@ptr, :nextrepl=>clast}) if $VERBOSE
+        @pos -= clast[:ndesc]
+        p({:nextrepl=>clast, :pos=>@pos}) if $VERBOSE
       end
-    else
-        p({:newptr=>@ptr, :ctos=>clast}) if $VERBOSE
     end
     d
   end
@@ -38,10 +63,14 @@ class BufrDecode
     printf "%03u %6s %15s # %s\n", desc[:pos], desc[:fxy], val.inspect, desc[:desc]
   end
 
+=begin
+記述子列がチューリングマシンのテープであるかのように走査して処理する。
+要素記述子を読むたびに、BUFR報 bufrmsg から実データを読み出す。
+=end
+
   def run out = $stdout
-    @ptr = 0
-    while @ptr < @tape.size
-      desc = read_tape
+    rewind
+    while desc = read_tape
       case desc[:type]
       when :str
         str = @bufrmsg.readstr(desc)
@@ -53,19 +82,24 @@ class BufrDecode
         r = desc.dup
         r[:ctr] = r[:ndesc] + 1
         @cstack.push r
-        showval r
+        showval r, :REPLICATION
         if r[:niter].zero? then
-          d = read_tape
+          d = read_tape(:keep_ctr)
           unless d and d[:type] == :num and /^031/ === d[:fxy]
-            raise "missing class 31 after delayed repl #{r.inspect}"
+            raise "class 31 must follow delayed replication #{r.inspect}"
           end
           num = @bufrmsg.readnum(d)
           showval d, num
           if num.zero? then
             r[:niter] = r[:ctr] = 1
+            forward(r[:ndesc])
           else
             r[:niter] = num
           end
+        end
+        if $VERBOSE
+          printf("loop pos=%u niter=%u ctr=%u ndesc=%u\n", @pos, r[:niter], r[:ctr],
+            r[:ndesc])
         end
       end
     end
@@ -77,6 +111,10 @@ class BufrDB
 
   class ENOSYS < BUFRMsg::ENOSYS
   end
+
+=begin
+BUFR表BおよびDを読み込む。さしあたり、カナダ気象局の libECBUFR 付属の表が扱いやすい形式なのでこれを用いる。
+=end
 
   def initialize table_b, table_d
     @b = {}
@@ -108,6 +146,12 @@ class BufrDB
     }
   end
 
+=begin
+記述子文字列の配列を受け取り、集約記述子を再帰的に展開し、反復記述子の修飾範囲を解析する。
+出力は集約と反復が配列中配列の木構造で表現されている。
+読みやすいように、集約の先頭には元の集約記述子が "#" を前置して置かれているので、 flatten してから # で始まるものを除去すれば、実際にインタプリタが駆動できるような記述子列が得られる。しかし flatten すると木構造での反復範囲の表現がわからなくなってしまうので、反復記述子は付け替えてある。
+=end
+
   def expand descs
     result = []
     a = descs.dup
@@ -121,6 +165,7 @@ class BufrDB
         x += 1 if y.zero?
         rep = expand(a.shift(x))
         newx = rep.flatten.reject{|s|/^#/ === s}.size
+        newx -= 1 if y.zero?
         rep.unshift format('1%02u%03u:%s', newx, y, fxy)
         result.push rep
       when /^3/ then
