50acb6056929f2f7e7bcf818aa6871ed3fa88f15
TOYODA Eizi <toyoda.eizi@gmail.com>
2019-11-05 11:49:14 +0000

    growing - bufr2temp now can encode section 1

--- bufr2temp.rb
+++ bufr2temp.rb
@@ -58,6 +58,21 @@ class Bufr2temp
     return nil
   end
 
+  def branch tree, nth = 0
+    tree.size.times{|i|
+      elem = tree[i]
+      case elem.first
+      when /^1/
+        if nth <= 0 then
+          i += 1 if /^031/ === tree[i+1].first
+          return tree[i + 1]
+        end
+        nth -= 1
+      end
+    }
+    return nil
+  end
+
   def itoa1 ival
     case ival when 0..9 then format('%01u', ival) else '/' end
   end
@@ -74,6 +89,49 @@ class Bufr2temp
     case ival when 0..9999 then format('%04u', ival) else '////' end
   end
 
+  def levsort levbranch
+    r = {}
+    levbranch.each{|levset|
+      flags = find(levset, '008042')
+      pres = find(levset, '007004')
+      pres = (pres / 100).to_i if pres
+      r[pres] = levset if 0 != (flags & 0x10000)
+      r[:SURF] = levset if 0 != (flags & 0x20000)
+      r[:MAXW] = levset if 0 != (flags & 0x04000)
+      if 0 != (flags & 0x08000) then
+        if r[:TRP1] then
+          r[:TRP2] = levset
+        else 
+          r[:TRP1] = levset
+        end
+      end
+    }
+    return r
+  end
+
+  STDLEVS = [
+    [:SURF, '99'],
+    [1000, '00'],
+    [925, '92'],
+    [850, '85'],
+    [700, '70'],
+    [500, '50'],
+    [400, '40'],
+    [300, '30'],
+    [250, '20'],
+    [200, '25'],
+    [150, '15'],
+    [100, '10']
+  ]
+
+  def toplevel levs
+    r = nil
+    for pres, id in STDLEVS
+      r = id if levs.include?(pres)
+    end
+    return r
+  end
+
   def subset tree
     print_ahl
     report = []
@@ -86,10 +144,69 @@ class Bufr2temp
     iii = find(tree, '001002')
     report.push [itoa2(_II), itoa3(iii)].join
 
+    levbranch = branch(tree, 0)
+    levels = levsort(levbranch)
+
     # YYGGId
-    id = '1' # toriaezu
+    id = '1'  # do not omit ddfff until 100 hPa
     yygg = @reftime.strftime('%d%H')
     report.push [yygg, id].join
+
+    # Section 2
+    STDLEVS.each{|pres, pp|
+      levset = levels[pres]
+
+      # 99PPP or PPhhh
+      case pres
+      when :SURF
+        ppp0 = levset ? find(levset, '007004') : nil
+        ppp0 = (ppp0 / 100) % 1000 if ppp0
+        report.push [pp, itoa3(ppp0)].join
+      else
+        hhh = levset ? find(levset, '010009') : nil
+        if hhh and hhh < 0 then
+          hhh = (hhh + 500.5).to_i
+        elsif hhh and pres > 500 then
+          hhh = (hhh * 10).to_i % 1000
+        elsif hhh then
+          hhh = (hhh + 0.5).to_i % 1000
+        end
+        report.push [pp, itoa3(hhh)].join
+      end
+
+      # TTTaDD
+      ttt = levset ? find(levset, '012101') : nil
+      td = levset ? find(levset, '012103') : nil
+      _DD = (ttt && td) ? ttt - td : nil
+      if ttt and ttt >= 273.15
+        ttt = ((ttt - 273.15) * 5).to_i * 2
+      elsif ttt
+        ttt = ((273.15 - ttt) * 5).to_i * 2 + 1
+      end
+      if _DD and _DD <= 5.0 then
+        _DD = (_DD * 10).to_i
+      elsif _DD then
+        _DD = (_DD + 0.5).to_i
+      end
+      report.push [itoa3(ttt), itoa2(_DD)].join
+
+      # ddfff
+      dd = levset ? find(levset, '011001') : nil
+      fff = levset ? find(levset, '011002') : nil
+      dd = ((dd + 2.5) / 5).to_i * 5 if dd
+      fff = (fff + 0.5).to_i if fff
+      if dd and 0 != (dd % 10) then
+        fff += 500 if fff
+      end
+      dd /= 10 if dd
+      report.push [itoa2(dd), itoa3(fff)].join
+    }
+
+=begin
+    report.push '61616 DEBUG'
+    maxws = branch(tree, 1)
+    report.push "MAXW#{maxws ? maxws.size : '/'}"
+=end
 
     @out.puts report.join(' ') + "=\r\r"
   end
