5a616987490d81c429cad12708b1470b64f97a49
TOYODA Eizi <toyoda.eizi@gmail.com>
2019-11-08 00:22:58 +0000

    feature: BSO output implemented

--- output.rb
+++ output.rb
@@ -3,10 +3,9 @@
 class Output
 
   def initialize cfgstr = ''
-    @file = nil
+    @file = @fmt = nil
     @cccc = 'RJXX'
-    @fmt = :ia2
-    cfgstr.strip.split(/,/).each{|spec|
+    cfgstr.strip.sub(/^-o/, '').split(/,/).each{|spec|
       case spec
       when /^FMT=(.+)/i then @fmt = $1.to_sym
       when /^FILE=(.+)/i then @file = $1
@@ -15,34 +14,44 @@ class Output
       else @file = spec
       end
     }
-    @fp = @file ? File.open(@file, 'wb') : $stdout
-    @fmt = :ia2 if @fp.tty?
+    if @fmt.nil? then
+      @fmt = if @file then :BSO else :IA2 end
+    end
+    @fp = @file ? File.open(@file, 'wb:BINARY') : $stdout
     # failsafe
     @buf = []
     @ahl = nil
     @n = 0
+    @ahldict = {}
   end
 
   def startmsg ttaaii, yygggg
+    @ahl = "#{ttaaii} #{@cccc} #{yygggg}"
+    if @ahldict.include? @ahl then
+      @ahl += ' RRA'
+      while @ahldict.include? @ahl and / RRX$/ !~ @ahl
+        @ahl = @ahl.succ
+      end
+    end
+    @ahldict[@ahl] = true
     case @fmt
-    when :ia2
-      ahl = "#{ttaaii} #{@cccc} #{yygggg}"
+    when :IA2
       raise Errno::EDOM, "too many msgs in FMT=ia2" if @n > 999
       nnn = format('%03u', @n)
-      @fp.puts "ZCZC #{nnn}     \r\r\n#{ahl}\r\r\n"
-    when :bso
-      @buf = []
+      @fp.puts "ZCZC #{nnn}     \r\r\n#{@ahl}\r\r\n"
+    when :BSO
+      @buf = ["\n", @ahl, "\n"]
     else raise Errno::ENOSYS, "fmt = #@fmt"
     end
     @n += 1
-    ahl
+    @ahl
   end
 
   def puts line
     case @fmt
-    when :ia2
+    when :IA2
       @fp.puts line
-    when :bso
+    when :BSO
       @buf.push line
     end
   end
@@ -61,15 +70,35 @@ class Output
     puts(buf.join(' ') + "\r\r\n") unless buf.empty?
   end
 
+  def makebch
+    ver = 0x35
+    btype = 0x10 # for alphanumeric msg
+    kind = 0x21 # domestic obs
+    anlen = 0 # for alphanumeric msg
+    naps = 0x20
+    bchpack = "CxxxxCCxxCnC x6 C"
+    bch = [ver, btype, kind, anlen, 0, naps, 0].pack(bchpack)
+    sum = bch.unpack('n10').inject(:+) & 0xFFFF_FFFF
+    while not (sum & 0xFFFF_0000).zero?
+      sum = (sum >> 16) + (sum & 0xFFFF)
+    end
+    sum = 0xFFFF & ~sum
+    [ver, btype, kind, anlen, sum, naps, 0].pack(bchpack)
+  end
+
   def flush
     case @fmt
-    when :ia2
+    when :IA2
       @fp.puts "\n\n\n\n\n\n\n\nNNNN\r\r\n"
       @fp.flush
-    when :bso
-      @fp.write @buf.join if @buf
+    when :BSO
+      @buf.unshift(makebch())
+      @buf.push "\x03"
+      msg = @buf.join
+      @buf = nil
+      @fp.write format('%08u00', msg.bytesize)
+      @fp.write msg
     end
-    @buf = nil
   end
 
 end
