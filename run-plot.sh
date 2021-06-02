724bb63667618c288f0803399bf89c40b77cda0a
TOYODA Eizi <toyoda.eizi@gmail.com>
2021-06-02 00:14:15 +0000

    描画面追加

--- run-plot.sh
+++ run-plot.sh
@@ -57,7 +57,7 @@ gpvtime=$(ruby -rtime -e 'puts((Time.parse(ARGV.first)-3600*6).utc.strftime("%Y%
 gpvbase=$(ruby -rtime -e 'puts(Time.parse(ARGV.first).utc.strftime("%Y%m%dT%H%MZ"))' $basetime)
 if test -d $nwp/p1/jmagrib/${gpvtime}
 then
-  for ve in msl_Pmsl p300_Z p300_WINDS p500_Z p500_T p500_rVOR p700_RH p700_VVPa p850_Z p850_papT p850_WINDS p925_Z p925_papT sfc_RAIN z10_WINDS
+  for ve in msl_Pmsl p100_WINDS p200_Z p200_WINDS p300_Z p300_WINDS p500_Z p500_T p500_rVOR p700_RH p700_VVPa p850_Z p850_papT p850_WINDS p925_Z p925_papT sfc_RAIN z10_WINDS
   do
     if test -f $nwp/p1/jmagrib/${gpvtime}/v${gpvbase}_f006_${ve}.png ; then
       ln -f    $nwp/p1/jmagrib/${gpvtime}/v${gpvbase}_f006_${ve}.png .
@@ -100,8 +100,9 @@ do
     fi
     if test -f v${gpvbase}_f006_p${pres}_Z.png ; then
       upropt="${upropt} -GPV2:v${gpvbase}_f006_p${pres}_Z.png"
-    elif test -f v${gpvbase}_f006_p${pres}_WINDS.png ; then
-      upropt="${upropt} -GPV2:v${gpvbase}_f006_p${pres}_WINDS.png"
+    fi
+    if test -f v${gpvbase}_f006_p${pres}_WINDS.png ; then
+      upropt="${upropt} -GPV3:v${gpvbase}_f006_p${pres}_WINDS.png"
     fi
     ;;
   700)
@@ -112,7 +113,7 @@ do
       upropt="${upropt} -GPV2:v${gpvbase}_f006_p${pres}_VVPa.png"
     fi
     ;;
-  300|500)
+  100|200|300|500)
     if test -f v${gpvbase}_f006_p${pres}_Z.png ; then
       upropt=-GPV1:v${gpvbase}_f006_p${pres}_Z.png
     fi
