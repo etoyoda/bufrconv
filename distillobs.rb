4fb05598abfd83db5c957737c2039579d286f138
TOYODA Eizi <toyoda.eizi@gmail.com>
2021-06-03 00:27:30 +0000

    出力結果の圧縮

--- distillobs.rb
+++ distillobs.rb
@@ -6,6 +6,14 @@ for line in ARGF
   body = JSON.parse(sbody)
   hdr = shdr.split(/\//, 3)
   id = [hdr[0], hdr[1], body['La'].to_i, body['Lo'].to_i].join('/')
+  body['La'] = (body['La'] * 100).floor * 0.01
+  body['Lo'] = (body['Lo'] * 100).floor * 0.01
+  for var in body.keys
+    next unless body.include?(var)
+    body.delete(var) if body[var].nil?
+  end
+  sbody = JSON.generate(body).gsub(/000000+\d,/, ',')
+  line = [shdr, sbody].join(' ')
   if not db.include?(id) then
     db[id] = [hdr[2], line]
   elsif hdr[2] < db[id][0] then
