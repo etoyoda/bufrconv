38e8147724d5142703c1ad1c08e854b39b8bc098
TOYODA Eizi <toyoda.eizi@gmail.com>
2022-05-15 11:24:45 +0900

    デバグ(tvがちゃんと集計されていなかった)、直近ケッソク日時の記録

--- stnstat.rb
+++ stnstat.rb
@@ -10,8 +10,9 @@ def diag str
   STDERR.puts str if STDERR.tty?
 end
 
-time = Time.parse(ARGV.shift)
-stime = time.utc.strftime('%Y-%m-%dT%H:%MZ')
+time = Time.parse(ARGV.shift).utc
+stime = time.strftime('%Y-%m-%dT%H:%MZ')
+shour = time.strftime('%H')
 diag "select time #{stime}"
 
 db = Hash.new
@@ -31,23 +32,32 @@ for line in ARGF
   next if /^gpv/ === istn
   data = JSON.parse(jdata)
   unless db[istn]
-    db[istn] = {'@'=>istn, 'tf'=>stime, 'av'=>1.0, 'at'=>nil,
-      'tv'=>1.0, 'tt'=>nil, 'pv'=>1.0, 'pt'=>nil, }
+    db[istn] = {'@'=>istn, 'ent'=>stime,
+      'av'=>1.0, 'at'=>nil,
+      'am00'=>nil, 'am03'=>nil, 'am06'=>nil, 'am09'=>nil,
+      'am12'=>nil, 'am15'=>nil, 'am18'=>nil, 'am21'=>nil,
+      'tv'=>0.0, 'tt'=>nil, 'tm'=>nil,
+      'pv'=>0.0, 'pt'=>nil, 'pm'=>nil,
+    }
   end
   ent = db[istn]
   ent['at'] = stime
   ent['av'] = ent['av'] * (1.0 - VFACTOR) + 1.0 * VFACTOR
   ent['tt'] = stime if data['T']
-  ent['av'] = ent['av'] * (1.0 - VFACTOR) + (data['T'] ? 1.0 : 0.0) * VFACTOR
+  ent['tm'] = stime unless data['T']
+  ent['tv'] = ent['tv'] * (1.0 - VFACTOR) + (data['T'] ? 1.0 : 0.0) * VFACTOR
   ent['pt'] = stime if data['P']
+  ent['pm'] = stime unless data['P']
   ent['pv'] = ent['pv'] * (1.0 - VFACTOR) + (data['P'] ? 1.0 : 0.0) * VFACTOR
 end
 
 for stn in db.keys
   next if db[stn]['at'] == stime
-  ent['av'] = ent['av'] * (1.0 - VFACTOR)
-  ent['tv'] = ent['tv'] * (1.0 - VFACTOR)
-  ent['pv'] = ent['pv'] * (1.0 - VFACTOR)
+  ent = db[stn]
+  ent['am'+shour] = stime
+  ent['av'] *= (1.0 - VFACTOR)
+  ent['tv'] *= (1.0 - VFACTOR)
+  ent['pv'] *= (1.0 - VFACTOR)
 end
 
 for stn in db.keys
