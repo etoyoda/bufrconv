47353706aa52dc010c7b398e9fe3b55170d5b261
TOYODA Eizi <toyoda.eizi@gmail.com>
2026-05-27 00:12:27 +0000

    crontab 登録機能追加

--- deploy-ffnwp.sh
+++ deploy-ffnwp.sh
@@ -2,3 +2,4 @@
 set -Ceuxo pipefail
 test -d /nwp/bin
 sudo -u nwp cp run-plot.sh bufrsort distillobs.rb sort2sfcmap.rb sort2uprmap.rb table_* bufr2pick /nwp/bin
+sudo install -m 0644 crontab.txt /etc/cron.d/bufrconv
